a94234d83fe4b56974d4c260cc8d1029f3e98e5b
wonseokyoon <01088115030a@gmail.com>
2024-12-19 20:00:13 +0900

    create end, register

---
+++ src/main/java/Quote.java
@@ -0,0 +1,11 @@
+public class Quote {
+    int id;
+    String content;
+    String author;
+
+    Quote(int id,String content, String author){
+        this.id=id;
+        this.content=content;
+        this.author=author;
+    }
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,51 @@
+
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//
+//
+//
+//
+
+public class main {
+    private static final List<Quote> quotes=new ArrayList<>();
+    private static int nextId=1;
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        while(true){
+            System.out.println("==명언 앱==");
+            System.out.print("명령) ");
+            String command = scanner.nextLine().trim();
+
+            if(command.equals("종료")){
+                break;
+            } else if (command.startsWith("등록")) {
+                registerQuote(scanner);
+            }
+        }
+        scanner.close();    //리소스해제
+    }
+
+    private static void registerQuote(Scanner scanner){ //등록 메서드
+        System.out.print("명언: ");
+        String content=scanner.nextLine().trim();
+        System.out.print("작가: ");
+        String author=scanner.nextLine().trim();
+
+        Quote quote=new Quote(nextId++,content,author);
+        quotes.add(quote);
+
+        System.out.println(quote.id+"번 명언이 등록되었습니다.");
+    }
+
+
+}
